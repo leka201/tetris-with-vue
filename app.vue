0a9e650738f6fc308f4be07340bc88a4a09de7fe
leka201 <alexiaizabelasantos@hotmail.com>
2024-10-02 09:11:42 -0300

    tentando consertar erros

---
+++ app.vue
@@ -0,0 +1,42 @@
+<script setup>
+
+import Tetris from './components/Tetris.vue'
+import { onMounted} from 'vue';
+
+onMounted(() => {
+    document.title = 'Tetris With Vue| Leka201' 
+
+      const canvas = document.getElementById("tetris");
+      const context = canvas.getContext("2d");
+      context.scale(20, 20);
+
+   });
+
+</script>
+
+<template>
+   
+   <Tetris/>    
+   <div id= "score"></div>
+   <canvas id = "tetris" width ="240" height="400"></canvas>
+
+</template>
+
+<style>
+
+body{
+   background: #202028; 
+   color: #fff;
+   font-family: sans-serif;
+   font-size: 2em;
+   text-align: center;
+}
+
+canvas{
+   border: solid 0.2em #fff; 
+   height: 90vh;
+}
+
+</style>
+
+
